16c23776515847afc531809fdf6027abd0dabf79
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-10-31 12:02:24 +0100

    Create maven project

---
+++ src/main/java/de/allianz/project3110/Project3110Application.java
@@ -0,0 +1,13 @@
+package de.allianz.project3110;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3110Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3110Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/project3110/Project3110ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.project3110;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3110ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
